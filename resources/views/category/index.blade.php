faf2ef8ba1a7abd6fdf05914a586adda04f35041
xVAPORIZEx <78992801+xVAPORIZEx@users.noreply.github.com>
2021-02-13 16:10:32 +0700

    ทำ category ได้แล้วววว

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.master')
+
+@section('content')
+    <h1>หมวดหมู่ข่าว</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <td>ลำดับ</td>
+                <td>ชื่อหมวดหมู่</td>
+                <td>สร้างเมื่อ</td>
+                <td>จัดการ</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($categories as $item)
+            <tr>
+                <td>{{$item->id}}</td>
+                <td>{{$item->name}}</td>
+                <td>{{$item->created_at}}</td>
+                <td>แก้ไข | ลบ</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
